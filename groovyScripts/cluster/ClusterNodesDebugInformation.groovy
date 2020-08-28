50abd429a5a3fbecc676c87f88ca36218e0cf545
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2020-08-28 12:17:55 +0200

    Groovy script that shows the different nodes that currently are part of the cluster.

---
+++ groovyScripts/cluster/ClusterNodesDebugInformation.groovy
@@ -0,0 +1,51 @@
+import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
+import com.liferay.portal.kernel.cluster.ClusterNode;
+import com.liferay.portal.kernel.cluster.ClusterMasterExecutorUtil;
+
+import java.util.List;
+
+class ClusterNodesDebugInformation {
+
+    ClusterNodesDebugInformation() {
+        isMaster = ClusterMasterExecutorUtil.isMaster();
+    }
+
+    void printDebugInformation(PrintWriter out) {
+        out.println("================================================================");
+        out.println("=== Master: "+ isMaster);
+        out.println("=== " + new Date());
+
+        out.println("================================================================");
+        out.println("=== Current Cluster Node: ");
+        printClusterNodeInformation(ClusterExecutorUtil.getLocalClusterNode(), out);
+
+        List<ClusterNode> clusterNodes = ClusterExecutorUtil.getClusterNodes();
+
+        out.println("================================================================");
+        out.println("=== Total cluster nodes: " + clusterNodes.size());
+
+        int i = 0;
+
+        for(ClusterNode clusterNode: clusterNodes) {
+            out.println("================================================================");
+            out.println("=== Cluster Node # " + i);
+            
+            printClusterNodeInformation(clusterNode, out);
+
+            i++;
+        }
+        
+    }
+
+    private void printClusterNodeInformation(ClusterNode clusterNode, PrintWriter out) {
+        out.println("BindInetAddress: " + clusterNode.getBindInetAddress());
+        out.println("ClusterNodeId: " + clusterNode.getClusterNodeId());
+        out.println("PortalInetSocketAddress: " + clusterNode.getPortalInetSocketAddress());
+        out.println("PortalProtocol: " + clusterNode.getPortalProtocol());
+    }
+
+    private final boolean isMaster;
+}
+
+(new ClusterNodesDebugInformation()).printDebugInformation(out);
+
